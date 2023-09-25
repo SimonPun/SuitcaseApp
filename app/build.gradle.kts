7501704be43b275c2c1913922c38be90b566bdae
simon pun <mgrsimon333@gmail.com>
2023-09-25 10:00:51 +0545

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.squareup.picasso:picasso:2.71828")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -38,6 +38,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.7.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.google.firebase:firebase-database:20.2.2")
+    implementation("com.google.firebase:firebase-storage:20.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -47,9 +48,4 @@ dependencies {
     implementation ("androidx.recyclerview:recyclerview:your_recyclerview_version")
     implementation ("androidx.cardview:cardview:your_cardview_version")
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-
-
-
-
 }
